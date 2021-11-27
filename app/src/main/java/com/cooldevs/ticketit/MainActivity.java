7a26d9f2c1bdbd8eede1ab847ecf7d6383c7c3b8
Harsh Gujarathi <gujarathiharsh02@gmail.com>
2021-11-27 17:00:08 +0530

    Added Admin check

--- app/src/main/java/com/cooldevs/ticketit/MainActivity.java
+++ app/src/main/java/com/cooldevs/ticketit/MainActivity.java
@@ -38,23 +38,25 @@ public class MainActivity extends AppCompatActivity {
     fStore=FirebaseFirestore.getInstance();
     userId= fAuth.getCurrentUser().getUid();
 
+
         DocumentReference documentReference = fStore.collection("users").document(userId);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                 u1 =new User(value.getString("fName"),value.getString("email"),value.getString("phoneNo"),value.getBoolean("admin"),value.getString("username"));
 
+
                 fname.setText("Welcome! " + u1.fname);
 
 
-                if (u1.admin == true)
-                {
-                    adminBtn.setVisibility(View.VISIBLE);
-                }
-                else
-                {
-                    adminBtn.setVisibility(View.GONE);
-                }
+//                if (u1.admin)
+//                {
+//                    adminBtn.setVisibility(View.VISIBLE);
+//                }
+//                else
+//                {
+//                    adminBtn.setVisibility(View.GONE);
+//                }
             }
 
         });
