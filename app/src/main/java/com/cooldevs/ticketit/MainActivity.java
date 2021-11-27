dfc6860cb0125d9b607e6aa12f6c2c224b4366d3
Harsh Gujarathi <gujarathiharsh02@gmail.com>
2021-11-27 15:55:24 +0530

    Added Welcome Text

--- app/src/main/java/com/cooldevs/ticketit/MainActivity.java
+++ app/src/main/java/com/cooldevs/ticketit/MainActivity.java
@@ -1,18 +1,28 @@
 package com.cooldevs.ticketit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
     Button logoutBTn,bookTicket,cancelBtn,adminBtn;
-
+    TextView fname;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    Boolean adminCheck;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     bookTicket=findViewById(R.id.book_btn);
     cancelBtn=findViewById(R.id.cancel_btn);
     adminBtn = findViewById(R.id.admin_btn);
+    fname=findViewById(R.id.fname_text);
+    fStore=FirebaseFirestore.getInstance();
+    userId= fAuth.getCurrentUser().getUid();
+    adminCheck=false;
     bookTicket.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -41,11 +55,32 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
+    DocumentReference documentReference = fStore.collection("users").document(userId);
+    documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+        @Override
+        public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+            fname.setText("Welcome! " + value.getString("fName"));
+
+            adminCheck=value.getBoolean("admin");
+            if (adminCheck == true)
+            {
+                adminBtn.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                adminBtn.setVisibility(View.GONE);
+            }
+        }
+
+    });
+
+
     logoutBTn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             fAuth.signOut();
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            finish();
         }
 
 
